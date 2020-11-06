3dcf5803d119f638e6f2b5227a726ffef759b9f6
Thomas Zennaro <thomas.zennaro.1@ens.etsmtl.ca>
2020-11-06 17:20:16 +0100

    light service

---
+++ LightManagementWS/LightManagementWS/src/test/java/fr/insa/soa/LightManagementWS/LightManagementWsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.soa.LightManagementWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightManagementWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
