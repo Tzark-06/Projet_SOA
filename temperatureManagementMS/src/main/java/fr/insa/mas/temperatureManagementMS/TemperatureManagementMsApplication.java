ad792db455a16c16b391062b0766081113835125
vi-ct-or <58001038+vi-ct-or@users.noreply.github.com>
2020-11-08 23:22:34 +0100

    Add files via upload

---
+++ temperatureManagementMS/src/main/java/fr/insa/mas/temperatureManagementMS/TemperatureManagementMsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.mas.temperatureManagementMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureManagementMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureManagementMsApplication.class, args);
+	}
+
+}
