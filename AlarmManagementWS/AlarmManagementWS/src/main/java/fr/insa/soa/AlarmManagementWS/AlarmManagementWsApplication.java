00a009ef3cba1f9151d34c752d09acdd7a6478e8
Thomas Zennaro <thomas.zennaro.1@ens.etsmtl.ca>
2020-11-18 18:03:48 +0100

    alarmManagement

---
+++ AlarmManagementWS/AlarmManagementWS/src/main/java/fr/insa/soa/AlarmManagementWS/AlarmManagementWsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.soa.AlarmManagementWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmManagementWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmManagementWsApplication.class, args);
+	}
+
+}
