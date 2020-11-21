07dce25d6ba8a2c5cbfbc5048ce7ea1c274c666b
victorleroch <leroch@etud.insa-toulouse.fr>
2020-11-21 11:15:16 +0100

    presence Managment ajouté

---
+++ presenceManagementMS/src/test/java/fr/insa/mas/presenceManagementMS/PresenceManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.mas.presenceManagementMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresenceManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
